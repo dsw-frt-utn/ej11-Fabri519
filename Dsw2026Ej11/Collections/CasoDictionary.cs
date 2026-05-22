07e5dd054780f0fb143f0f7494619cbba84d6a0f
Fabri519 <fabrizzio.lopezroldan@alu.frt.utn.edu.ar>
2026-05-22 00:55:51 -0300

    Add CasoDic/CasoList

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un diccionario donde la clave sea el legajo y el valor el alumno
 //Incluir un método para agregar un alumno al diccionario
@@ -7,4 +9,10 @@
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+    private Dictionary<int, Alumno> dic = [];
+    public void AgregarAlumno(int legajo, Alumno alumno) => dic.Add(legajo, alumno);
+    public Alumno? BuscarAlumno(int legajo) => dic.TryGetValue(legajo, out var alumno) ? alumno : null;
+    public Dictionary<int, Alumno> ObtenerDiccionario() => dic;
+    public void EliminarAlumno(int legajo) => dic.Remove(legajo);
+
 }
